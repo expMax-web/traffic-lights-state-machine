6e7e61208211eb00745b43fc8b05a299bd125d3d
Maksim Timofeev <maxf.f2014@yandex.ru>
2022-11-13 18:24:56 +0400

    fix TraficLights

--- src/component/TraficLights/TraficLights.tsx
+++ src/component/TraficLights/TraficLights.tsx
@@ -1,12 +1,23 @@
-import { FC } from "react";
+import { FC, useReducer } from "react";
+import { TRAFIC_LIGHTS_ITEMS_INITIAL_STATE } from "./constants";
+import traficLightsReducer from "./reducers";
 
 import styles from './styles.module.scss'
+import { TraficLightsButton } from "./TraficLightsButton";
 import { TraficLightsItem } from "./TraficLightsItem";
 
 export const TraficLights: FC = () => {
-    return <div className={styles.Container} >
-        <TraficLightsItem color="green" />
-        <TraficLightsItem color="yellow" />
-        <TraficLightsItem color="red" />
-    </div>
+
+    const [traficLightsItemsState, dispatch] = useReducer(traficLightsReducer, TRAFIC_LIGHTS_ITEMS_INITIAL_STATE);
+
+    return (
+        <div className={styles.Container}>
+            <div className={styles.TraficLights} >
+                {traficLightsItemsState.map((item) => {
+                    return <TraficLightsItem color={item.color} isActive={item.isActive} key={item.color} />
+                })}
+            </div>
+            <TraficLightsButton onClick={() => dispatch({ type: 'SWITCH_COLOR' })} />
+        </div>
+    )
 }
