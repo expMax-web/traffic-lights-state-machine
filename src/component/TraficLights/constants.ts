59befdce7e4aff0f74f0267bea81a57e2b22a53c
Maksim Timofeev <maxf.f2014@yandex.ru>
2022-11-13 18:24:13 +0400

    add TRAFIC_LIGHTS_ITEMS_INITIAL_STATE

---
+++ src/component/TraficLights/constants.ts
@@ -0,0 +1,7 @@
+import { TraficLightsItemsState } from "./types";
+
+export const TRAFIC_LIGHTS_ITEMS_INITIAL_STATE: TraficLightsItemsState = [
+  { color: "red", isActive: true },
+  { color: "yellow", isActive: false },
+  { color: "green", isActive: false },
+];
