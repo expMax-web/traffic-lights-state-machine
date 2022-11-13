19b81239e894fbcd091f63ae6bd19a485ca6a58c
Maksim Timofeev <maxf.f2014@yandex.ru>
2022-11-13 18:25:06 +0400

    add types

--- src/component/TraficLights/types.ts
+++ src/component/TraficLights/types.ts
@@ -1,1 +1,7 @@
 export type Color = "green" | "yellow" | "red";
+
+export type TraficLightsActions = { type: "SWITCH_COLOR" };
+
+export type TraficLightsItemState = { color: Color; isActive: boolean };
+
+export type TraficLightsItemsState = TraficLightsItemState[];
