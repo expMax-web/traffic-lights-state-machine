a1e8a6b4b40d2e24eb1b7670f65749fce5f62ac5
Maksim Timofeev <maxf.f2014@yandex.ru>
2022-11-13 18:24:49 +0400

    fix TraficLightsItem

--- src/component/TraficLights/TraficLightsItem.tsx
+++ src/component/TraficLights/TraficLightsItem.tsx
@@ -1,29 +1,31 @@
 import { FC } from "react"
+import cn from "classnames"
 
 import { Color } from "./types"
 
 import styles from './styles.module.scss'
 
 
+
 interface TraficLightsItemProps {
-    color: Color
+    color: Color;
+    isActive: boolean;
 }
 
-const COLOR_ITEM_MAP: Record<Color, string> = {
-    red: "Item_Red",
-    yellow: "Item_Red",
-    green: "Item_Red",
+const COLOR_ITEM_CLASSNAME_MAP: Record<Color, string> = {
+    green: styles.Item_Green,
+    red: styles.Item_Red,
+    yellow: styles.Item_Yellow,
 }
 
 
 
 
 
-export const TraficLightsItem: FC<TraficLightsItemProps> = ({ color }) => {
-    return <div className={styles.Item} />
-}
+export const TraficLightsItem: FC<TraficLightsItemProps> = ({ color, isActive }) => {
 
-// {cn(styles.Button, {
-//     [styles.Button_Active]:
-//       bookingComponent === BookingComponentsEnum.bookingForm,
-//   })}
+    const currentColor = COLOR_ITEM_CLASSNAME_MAP[color]
+
+    return <div className={cn(styles.Item, currentColor,
+        { [styles.Item_Active]: isActive })} />
+}
